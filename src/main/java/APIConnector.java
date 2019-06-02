3eff89cb305e208f5a8e67a2d596f1007f505234
Vicky M <s0558484@htw-berlin.de>
2019-06-02 16:37:12 +0200

    Created APIConnector

---
+++ src/main/java/APIConnector.java
@@ -0,0 +1,93 @@
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class APIConnector {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    //5ce2722730705e001adf023a-SENSEBOX_EDU_PROJEKTSTUDIUM_OSD_HTW_BERLIN_SOSE2019
+
+    void sendGet() throws Exception {
+
+        String sensorID = "5ce2722730705e001adf023a";
+
+        String url = "https://api.opensensemap.org/boxes/"+sensorID+ "?format=json";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+    // HTTP POST request
+    void sendPost() throws Exception {
+
+        String url = "https://selfsolve.apple.com/wcResults.do";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+}
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
+        APIConnector ac = new APIConnector();
+
+
+        try {
+        ac.sendGet();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 }
