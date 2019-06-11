a695f1084d6176cd4b9dba9da092da9439c784d6
Roman <s0547499@htw-berlin.de>
2019-06-11 23:29:09 +0200

    implemented SenseBox to Statements parsing

--- src/main/java/util/RdfParser.java
+++ src/main/java/util/RdfParser.java
@@ -1,13 +1,25 @@
 package util;
 
+import models.Sensor;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
 import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.GEO;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
 import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFParser;
 import org.eclipse.rdf4j.rio.RDFWriter;
 import org.eclipse.rdf4j.rio.Rio;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
+
+import models.SenseBox;
+import vocabularys.SOSA;
 
 public class RdfParser {
 
@@ -15,6 +27,8 @@ public class RdfParser {
         FileOutputStream stream = new FileOutputStream(filePath);
 
         RDFWriter writer = Rio.createWriter(format, stream);
+
+        RDFParser parser = Rio.createParser(format);
 
         writer.startRDF();
 
@@ -24,5 +38,43 @@ public class RdfParser {
 
         writer.endRDF();
         stream.close();
+    }
+
+    public static List<Statement> SenseBoxToStatements(SenseBox senseBox) {
+        List<Statement> statements = new LinkedList<Statement>();
+
+        ValueFactory factory = SimpleValueFactory.getInstance();
+
+        IRI senseBoxIri = factory.createIRI("SenseBox:" + senseBox.getId());
+        statements.add(factory.createStatement(senseBoxIri, RDF.TYPE, SOSA.Platform));
+
+        for (Sensor sensor : senseBox.getBuiltInSensors()) {
+            IRI sensorIri = factory.createIRI("Sensor:" + sensor.getSensorID());
+            statements.add(factory.createStatement(senseBoxIri, SOSA.hosts, sensorIri));
+            statements.add(factory.createStatement(sensorIri, RDF.TYPE, SOSA.Sensor));
+
+            Literal sensorTitle = factory.createLiteral(sensor.getTitle());
+            statements.add(factory.createStatement(sensorIri, RDFS.LABEL, sensorTitle));
+
+            IRI sensorResultIri = factory.createIRI("Result:" + sensor.getSensorID() + "_" + sensor.getLastMeasurement().getCreatedAt());
+            statements.add(factory.createStatement(sensorResultIri, RDF.TYPE, SOSA.Observation));
+            statements.add(factory.createStatement(sensorResultIri, SOSA.madeBySensor, sensorIri));
+
+            IRI sensorResultUnit = factory.createIRI("Unit:" + sensor.getUnit());
+            Literal sensorResult = factory.createLiteral(String.valueOf(sensor.getLastMeasurement().getValue()), sensorResultUnit);
+            statements.add(factory.createStatement(sensorResultIri, SOSA.hasSimpleResult, sensorResult));
+        }
+
+        Literal senseBoxLocation = CoordinatesToLiteral(senseBox.getCurrentLocation().getCoordinates());
+        statements.add(factory.createStatement(senseBoxIri, GEO.WKT_LITERAL, senseBoxLocation));
+
+        Literal senseBoxTime = factory.createLiteral(senseBox.getLastMeasurementAt());
+        statements.add(factory.createStatement(senseBoxIri, SOSA.resultTime, senseBoxTime));
+
+        return statements;
+    }
+
+    private static Literal CoordinatesToLiteral(double[] coordinates) {
+        return SimpleValueFactory.getInstance().createLiteral("POINT(" + coordinates[0] + ", " + coordinates[1] + ")");
     }
 }
