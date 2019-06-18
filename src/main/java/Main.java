217cd845e00d52ac14f59b96c1a4a1d2550ce32f
Roman <s0547499@htw-berlin.de>
2019-06-18 10:50:12 +0200

    implemented cli support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,18 @@
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
+
+import osm.util.OpenSenseMapHandler;
+import util.Config;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException, IOException {
+
+        CommandLine config = new Config().parseArgs(args);
+
+        if (config.hasOption("osm")) {
+            new OpenSenseMapHandler().handleUserAction(config);
+        }
     }
 }
