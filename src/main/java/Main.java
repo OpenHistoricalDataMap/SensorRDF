205b20f4afba4764e8984c8fd73b33589a7701cf
Roman <s0547499@htw-berlin.de>
2019-07-26 15:21:23 +0200

    output for missing option, added working program call to readme

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.ParseException;
 
 import osm.util.OpenSenseMapHandler;
@@ -9,10 +10,18 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws ParseException, IOException {
 
-        CommandLine config = new Config().parseArgs(args);
+        try {
+            CommandLine config = new Config().parseArgs(args);
 
-        if (config.hasOption("osm")) {
-            new OpenSenseMapHandler().handleConfig(config);
+            if (config.hasOption("osm")) {
+                new OpenSenseMapHandler().handleConfig(config);
+            } else {
+                System.out.println("No data source specified.");
+                System.out.println("Please refer to the README.");
+            }
+        } catch (MissingOptionException e) {
+            System.out.println("You are missing the following options: " + e.getMissingOptions());
+            System.out.println("Please refer to the README.");
         }
     }
 }
